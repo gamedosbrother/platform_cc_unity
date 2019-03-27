769cc80d47b7cafabd288d988dbc8bce9e8711a9
Artur Morelle Nista <arturnista@gmail.com>
2019-03-26 22:39:48 -0300

    initial commit

---
+++ Assets/Player/PlayerLook.cs
@@ -0,0 +1,63 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerLook : MonoBehaviour
+{
+
+	[SerializeField]
+    [Range(0f, 5f)]
+    private float sensitivity = 3f;
+	[SerializeField]
+    private Vector2 minimum;
+	[SerializeField]
+    private Vector2 maximum;
+
+	private Vector2 rotation;
+	private Quaternion originalRotation;
+
+	private Camera camera;
+
+    void Awake()
+    {
+        originalRotation = transform.rotation;
+		Cursor.lockState = CursorLockMode.None;
+        
+        camera = Camera.main;
+    }
+
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+		if (Input.GetKeyDown (KeyCode.P)) {
+			if (Cursor.lockState == CursorLockMode.Locked) Cursor.lockState = CursorLockMode.None;
+			else Cursor.lockState = CursorLockMode.Locked;
+		}
+        
+		// Read the mouse input axis
+		rotation.x += Input.GetAxis ("Mouse X") * sensitivity;
+		rotation.y += Input.GetAxis ("Mouse Y") * sensitivity;
+
+		rotation.x = ClampAngle (rotation.x, minimum.x, maximum.x);
+		rotation.y = ClampAngle (rotation.y, minimum.y, maximum.y);
+
+		Quaternion xQuaternion = Quaternion.AngleAxis (rotation.x, Vector3.up);
+		Quaternion yQuaternion = Quaternion.AngleAxis (rotation.y, -Vector3.right);
+
+		transform.localRotation = originalRotation * xQuaternion;
+		camera.transform.localRotation = originalRotation * yQuaternion;
+    }
+
+	float ClampAngle (float angle, float min, float max)
+    {
+		if (angle < -360f) angle += 360f;
+		if (angle > 360f) angle -= 360f;
+		return Mathf.Clamp (angle, min, max);
+	}
+
+}
---
+++ Assets/Player/PlayerMovement.cs
@@ -0,0 +1,82 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+ 
+    [Header("Movement")]
+    [SerializeField]
+    private float moveSpeed;
+    [SerializeField]
+    private float acceleration;
+    [Header("Jump")]
+    [SerializeField]
+    private float jumpHeight;
+    [SerializeField]
+    private int airJumpsAmount = 1;
+
+    private float gravity;
+
+    private float jumpForce;
+    private int airJumpsPerformed = 0;
+
+    private Vector3 targetMoveVelocity;
+    private Vector3 moveVelocity;
+
+    private float verticalSpeed;
+
+    private CharacterController characterController;
+
+    void Awake()
+    {
+        characterController = GetComponent<CharacterController>();
+
+        gravity = 10f;
+        jumpForce = Mathf.Sqrt(jumpHeight * 2f * gravity);
+    }
+
+    void Update()
+    {
+        float verticalMovement = Input.GetAxisRaw("Vertical");
+        float horizontalMovement = Input.GetAxisRaw("Horizontal");
+
+        targetMoveVelocity = (transform.forward * moveSpeed * verticalMovement) + (transform.right * moveSpeed * horizontalMovement);
+
+        if(Input.GetKeyDown(KeyCode.Space))
+        {
+            if(characterController.isGrounded)
+            {
+                Jump();
+            }
+            else if(++airJumpsPerformed <= airJumpsAmount)
+            {
+                Jump();
+            }
+        }
+    }
+
+    void Jump()
+    {
+        verticalSpeed = jumpForce;
+    }
+
+    void FixedUpdate()
+    {
+        moveVelocity = Vector3.MoveTowards(moveVelocity, targetMoveVelocity, acceleration * Time.fixedDeltaTime);
+
+        verticalSpeed -= gravity * Time.fixedDeltaTime;
+
+        Vector3 velocity = moveVelocity;
+        velocity.y = verticalSpeed;
+
+        characterController.Move(velocity * Time.fixedDeltaTime);
+
+        if(characterController.isGrounded)
+        {
+            verticalSpeed = 0f;
+            airJumpsPerformed = 0;
+        }
+    }
+
+}
