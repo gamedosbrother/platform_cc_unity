c36c5ee2836983378f0a3263a196cc5bd0939991
mjrfornari <mjrfornari@gmail.com>
2019-03-29 21:37:27 -0300

    TransformFeature

---
+++ Assets/Player/PlayerTransformation.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class PlayerTransformation : MonoBehaviour
+{
+
+    private GameObject humanForm;
+    private GameObject creatureForm;
+
+    void Awake()
+    {
+        humanForm = GameObject.Find("HumanCapsule");
+        creatureForm = GameObject.Find("CreatureCapsule");
+        humanForm.SetActive(true);
+        creatureForm.SetActive(false);
+    }
+
+    void Update() 
+    {
+        if (Input.GetKeyDown(KeyCode.Alpha2)) 
+        {
+            humanForm.SetActive(false);
+            creatureForm.SetActive(true);
+        }
+
+        if (Input.GetKeyDown(KeyCode.Alpha1))
+        {
+            humanForm.SetActive(true);
+            creatureForm.SetActive(false);
+        }
+    }
+
+
+
+}
